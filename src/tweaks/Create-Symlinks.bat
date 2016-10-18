29b00f032148a8cc0e12b25720a502f89ee10c14
iRobie <github@doghairbakery.com>
2016-10-18 15:06:38 -0700

    Moved to dropbox

--- src/tweaks/Create-Symlinks.bat
+++ src/tweaks/Create-Symlinks.bat
@@ -1,4 +1,4 @@
 
 :: ConEmu settings
 del "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml"
-mklink /H "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml" "%userprofile%\OneDrive\Programs\Settings\ConEmu.xml" 
+mklink /H "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml" "%userprofile%\Dropbox\Programs\Settings\ConEmu.xml" 
