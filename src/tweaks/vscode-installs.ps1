0f1b3cba5478a4fee4afbc4a911fedbd1fe1b9a8
iRobie <github@doghairbakery.com>
2018-02-22 10:10:30 -0800

    Add files via upload

---
+++ src/tweaks/vscode-installs.ps1
@@ -0,0 +1,3 @@
+	code --install-extension robertohuertasm.vscode-icons # Go to File > Preferences > File Icon Theme > VSCode Icons.
+	code --install-extension ms-vscode.PowerShell
+	code --install-extension hnw.vscode-auto-open-markdown-preview
