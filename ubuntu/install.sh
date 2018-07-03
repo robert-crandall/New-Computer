a3df36550a289ec67328f0a5ed902ff4f5d7bf9b
iRobie <github@doghairbakery.com>
2018-07-03 08:37:20 -0700

    Create install.sh

---
+++ ubuntu/install.sh
@@ -0,0 +1,20 @@
+sudo apt-get update
+
+sudo apt install wget -y
+
+# Install Chrome Sources
+sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+
+## VS Code
+sudo add-apt-repository -y "deb https://packages.microsoft.com/repos/vscode stable main"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
+
+# Update and install files
+sudo apt update
+sudo apt-get -y install code google-chrome-stable git
+
+# Install Dropbox. Is there a Better way?
+wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb
+sudo apt-get install -o Acquire::ForceIPv4=true gdebi
+sudo gdebi dropbox_2015.10.28_amd64.deb -o Acquire::ForceIPv4=true
