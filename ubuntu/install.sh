d1b862ff74588de9c56fb407cc94e3b726bbab04
iRobie <github@doghairbakery.com>
2018-07-03 20:30:29 -0700

    Added some stuff

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 
-sudo apt install wget -y
+sudo apt install curl git wget npm -y
 
 # Install Chrome Sources
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
@@ -12,9 +12,16 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
 
 # Update and install files
 sudo apt update
-sudo apt-get -y install code google-chrome-stable git
+sudo apt-get -y install code google-chrome-stable
 
 # Install Dropbox. Is there a Better way?
 wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb
 sudo apt-get install -o Acquire::ForceIPv4=true gdebi
 sudo gdebi dropbox_2015.10.28_amd64.deb -o Acquire::ForceIPv4=true
+
+# Updating nodejs
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Teams:
+https://github.com/leftstick/teams-ubuntu
